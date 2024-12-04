4c7b18f1e519d2fa3e4981ebdd27b19cbc3e8398
Charlie3169 <77686049+Charlie3169@users.noreply.github.com>
2024-12-04 01:53:01 -0500

    Update entry.ts

--- src/scripts/entry.ts
+++ src/scripts/entry.ts
@@ -381,7 +381,7 @@ function createWorldEnvironment(): void
   );
 
   scene.add(
-    TextCreation.createTextSprite("Object Explorer Prototype 5", 
+    TextCreation.createTextSprite("Object Explorer Prototype 6", 
       1, 
       new THREE.Vector3(-20, 20, 20)
     )
