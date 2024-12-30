31a3821939eb21921fb7e163eadbd2689290050d
Charlie3169 <77686049+Charlie3169@users.noreply.github.com>
2024-12-30 00:37:22 -0500

    Update TestAPI.ts

--- src/scripts/services/TestAPI.ts
+++ src/scripts/services/TestAPI.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
 // Replace 'textrepository' with the service name defined in your docker-compose.yml
-const API_BASE_URL = 'http://api.orbscape.io'; //'http://10.0.0.15:3001'; //'http://api.orbscape.net';
+const API_BASE_URL = 'https://api.orbscape.io'; //'http://10.0.0.15:3001'; //'http://api.orbscape.net';
 
 
 
